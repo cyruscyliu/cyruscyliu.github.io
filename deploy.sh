6c2c4e0856a648f5db396f8362f00e984788a1fe
Qiang Liu <cyruscyliu@gmail.com>
2022-08-18 14:34:45 +0800

    change theme

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 # install
 # sudo apt install mkdocs
-# python3 -m pip install mkdocs mkdocs-bootstrap4
+# python3 -m pip install mkdocs mkdocs-bootstrap4 mkdocs-git-revision-date-localized-plugin mkdocs-material
 # local
 # mkdocs build
 # mkdocs serve
