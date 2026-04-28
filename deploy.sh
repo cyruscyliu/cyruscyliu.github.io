cd03c141dca905221df3c57e90cf14dd4d0e51b7
Qiang Liu <cyruscyliu@gmail.com>
2026-04-28 13:41:20 +0200

    deploy homepage with github pages actions
    Replace the old manual gh-pages workflow with a GitHub Actions Pages deploy and remove obsolete deployment helpers.

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-# install
-# sudo apt install mkdocs
-# python3 -m pip install mkdocs mkdocs-bootstrap4 mkdocs-git-revision-date-localized-plugin mkdocs-material
-# python3 -m pip install mkdocs-git-revision-date-plugin mkdocs-terminal
-# local
-# mkdocs build
-# mkdocs serve
-# remote
-mkdocs gh-deploy
